c7364a6b1e3f219158217b6198e2021ec232d383
daphnejo <101347819+daphnejo@users.noreply.github.com>
2026-08-22 13:40:04 +0500

    feat(mobile): register lesson eighteen screen

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -86,6 +86,7 @@ function RootNavigator() {
         <Stack.Screen name="lesson-fifteen" />
         <Stack.Screen name="lesson-sixteen" />
         <Stack.Screen name="lesson-seventeen" />
+        <Stack.Screen name="lesson-eighteen" />
         <Stack.Screen name="piano" options={{ orientation: 'landscape' }} />
         <Stack.Screen name="profile-info" />
         <Stack.Screen name="change-password" />
